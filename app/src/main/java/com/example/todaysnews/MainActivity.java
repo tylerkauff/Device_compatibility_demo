589d9ef993a0f500e8e7f81ecec0c7255c4fb499
tylerkauff <tyler.kauffman@cognizant.com>
2022-02-18 08:25:15 -0600

    Initial commit

---
+++ app/src/main/java/com/example/todaysnews/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.todaysnews;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.LinkedList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private final LinkedList<String> mWordList = new LinkedList<>();
+    private int mCount = 0;
+    private RecyclerView mRecyclerView;
+    private WordListAdapter mAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        for (int i = 0; i < 20; i++) {
+            mWordList.addLast("Word " + mCount++);
+            Log.d("WordList", mWordList.getLast());
+        }
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.recyclerview);
+        // Create an adapter
+        mAdapter = new WordListAdapter(this, mWordList);
+        // Connect the adapter
+        mRecyclerView.setAdapter(mAdapter);
+        // Give it a default layout manager
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int wordListSize = mWordList.size();
+                // Add a new word to the end of the wordList.
+                mWordList.addLast("+ Word " + wordListSize);
+                // Notify the adapter, that the data has changed so it can
+                // update the RecyclerView to display the data.
+                mRecyclerView.getAdapter().notifyItemInserted(wordListSize);
+                // Scroll to the bottom.
+                mRecyclerView.smoothScrollToPosition(wordListSize);
+            }
+        });
+    }
+}
